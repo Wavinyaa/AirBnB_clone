eb0a7e6b1472d7747ffac1ecfebc015e32396cd7
Wavinyaa <mitchellewavinya@gmail.com>
2023-02-12 10:54:21 -0800

    fixed

--- models/city.py
+++ models/city.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-"""
-creating class city
-"""
+"""Defines the City class."""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city
+    """Represent a city.
+
     Attributes:
         state_id (str): The state id.
-        name (str): The name of the city
+        name (str): The name of the city.
     """
 
-    state_id = ""
-    name = ""
-#!/usr/bin/python3
-"""
-City class, a subclass of BaseModel
-"""
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """
-    A subclass of BaseModel class
-    Public class attributes:
-        state_id: (str) will be State.id
-        name:     (str)
-    """
     state_id = ""
     name = ""
