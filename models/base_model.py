b07992a93fec97f71805a98b2a90e0f068ee05e0
Wavinyaa <111091226+Wavinyaa@users.noreply.github.com>
2023-02-12 21:02:32 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,75 +1,73 @@
 #!/usr/bin/python3
-from datetime import datetime
+"""
+Custom base class for the entire project
+"""
+
 from uuid import uuid4
+from datetime import datetime
 import models
 
-"""
-Parent class to all classes in the AirBnB clone project
-"""
-
-
-class BaseModel():
-    """Parent class for AirBnB clone project
+class BaseModel:
+    """Custom base for all the classes in the AirBnb console project
+    Arttributes:
+        id(str): handles unique user identity
+        created_at: assigns current datetime
+        updated_at: updates current datetime
     Methods:
-        __init__(self, *args, **kwargs)
-        __str__(self)
-        __save(self)
-        __repr__(self)
-        to_dict(self)
+        __str__: prints the class name, id, and creates dictionary
+        representations of the input values
+        save(self): updates instance arttributes with current datetime
+        to_dict(self): returns the dictionary values of the instance obj
     """
 
     def __init__(self, *args, **kwargs):
+        """Public instance artributes initialization
+        after creation
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
         """
-        Initialize attributes: uuid4, dates when class was created/updated
-        """
-        date_format = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs:
-            for key, value in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        date_format)
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        date_format)
-                elif "__class__" == key:
-                    pass
-                else:
-                    setattr(self, key, value)
-        else:
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """
-        Return class name, id, and the dictionary
+        Returns string representation of the class
         """
-        return ('[{}] ({}) {}'.
-                format(self.__class__.__name__, self.id, self.__dict__))
-
-    def __repr__(self):
-        """
-        returns string repr
-        """
-        return (self.__str__())
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """
-        Instance method to:
-        - update current datetime
-        - invoke save() function &
-        - save to serialized file
+        Updates the public instance attribute:
+        'updated_at' - with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
         """
-        Return dictionary of BaseModel with string formats of times
+        Method returns a dictionary containing all 
+        keys/values of __dict__ instance
         """
-        dic = self.__dict__.copy()
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        dic["__class__"] = self.__class__.__name__
-        return 
+        map_objects = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                map_objects[key] = value.isoformat()
+            else:
+                map_objects[key] = value
+        map_objects["__class__"] = self.__class__.__name__
+        return map_objects
